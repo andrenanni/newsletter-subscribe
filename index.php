d2efea1e827ad61bd3cdb2a5c873fbe8cdc014b3
andrenanni <andre-nanni@hotmail.com>
2025-08-25 17:40:27 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Newsletter - Subscribe</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <div class="box-subscribe">
+            <div class="content-left">
+                <img class="img-subscribe" src="/assets/asset_1.svg" alt="" srcset="">
+            </div>
+            <div class="content-right">
+                <div>
+                    <h2>Subscribe to our<br>Newsletter!</h2>
+                    <p>Subscribe to our newsletter and stay<br>updated.</p>
+                </div>
+                <div class="input">
+                    <input type="text" name="nome" id="" placeholder="Nome" >
+                    <input type="email" name="email" id="" placeholder="E-mail">
+                </div>
+                <button type="submit">Subscribe</button>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
